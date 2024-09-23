f5a81a26623ba9c42028a6dfa34a4718ce997a75
MinhPhamLapTrinh <116688660+MinhPhamLapTrinh@users.noreply.github.com>
2024-09-23 18:09:45 -0400

    stop the container when service stops

--- client/src/helper/krs-helpers.tsx
+++ client/src/helper/krs-helpers.tsx
@@ -1,56 +1,61 @@
-import { v1 } from "@docker/extension-api-client-types";
-
+import { v1 } from '@docker/extension-api-client-types';
 // Declare a variable to store the container ID
 let containerID: string;
+let kubeConfigPath: string;
 
 export const startKrsContainer = async (ddClient: v1.DockerDesktopClient) => {
   try {
     if (!containerID) {
       // Start the container in detached mode
-      const output = await ddClient.docker.cli.exec("run", [
-        "-d", // Detached mode to keep the container running
-        "--network",
-        "host", // Add the --network host option
-        "-v", // Volume flag
-        `~/.kube/config:/root/.kube/config`, // Mount the local ~/.kube/config to the container's /root/.kube/config
-        "dminhph/krs-extension:latest", // Docker image to run
-        "sleep",
-        "infinity",
+      const output = await ddClient.docker.cli.exec('run', [
+        '-d', // Detached mode to keep the container running
+        '--network',
+        'host', // Add the --network host option
+        '-v', // Volume flag
+        `${kubeConfigPath}:/root/.kube/config`, // Mount the local ~/.kube/config to the container's /root/.kube/config
+        'dminhph/krs-extension:latest', // Docker image to run
+        'sleep',
+        'infinity',
       ]);
       containerID = output.stdout.trim();
-      console.log("Container started with ID: ", containerID);
+      console.log('Container started with ID: ', containerID);
     }
 
     return containerID;
   } catch (error: any) {
-    console.error("Error starting KRS container: ", error);
+    console.error('Error starting KRS container: ', error);
     return `Failed to start KRS container: ${error.message}`;
   }
 };
 
-export const initKRS = async (ddClient: v1.DockerDesktopClient) => {
+export const initKRS = async (
+  ddClient: v1.DockerDesktopClient,
+  kubePath: string,
+) => {
   try {
+    kubeConfigPath = kubePath;
+
     // Ensure the container is running
     await startKrsContainer(ddClient);
 
     // Run the Docker command using the Docker Desktop API
-    const output = await ddClient.docker.cli.exec("exec", [
+    const output = await ddClient.docker.cli.exec('exec', [
       containerID,
-      "krs", // Command to run inside the container
-      "init", // Arguments to the krs command
+      'krs', // Command to run inside the container
+      'init', // Arguments to the krs command
     ]);
 
     // Log the output to see the results
-    console.log("Command executed, output received: ", output);
+    console.log('Command executed, output received: ', output);
 
     if (output?.stderr) {
-      console.error("Error output from krs init:", output.stderr);
+      console.error('Error output from krs init:', output.stderr);
       return `Error: ${output.stderr}`;
     }
 
-    return output?.stdout || "No output received";
+    return output?.stdout || 'No output received';
   } catch (error: any) {
-    console.error("Error executing krs command:", error);
+    console.error('Error executing krs command:', error);
     return `Failed to initialize krs: ${error.message}`;
   }
 };
@@ -61,23 +66,23 @@ export const krsScan = async (ddClient: v1.DockerDesktopClient) => {
     await startKrsContainer(ddClient);
 
     // Run the Docker command using the Docker Desktop API
-    const output = await ddClient.docker.cli.exec("exec", [
+    const output = await ddClient.docker.cli.exec('exec', [
       containerID,
-      "krs", // Command to run inside the container
-      "scan", // Arguments to the krs command
+      'krs', // Command to run inside the container
+      'scan', // Arguments to the krs command
     ]);
 
     // Log the output to see the results
-    console.log("Command executed, output received: ", output);
+    console.log('Command executed, output received: ', output);
 
     if (output?.stderr) {
-      console.error("Error output from krs scan:", output.stderr);
+      console.error('Error output from krs scan:', output.stderr);
       return `Error: ${output.stderr}`;
     }
 
-    return output?.stdout || "No output received";
+    return output?.stdout || 'No output received';
   } catch (error: any) {
-    console.error("Error executing krs command:", error);
+    console.error('Error executing krs command:', error);
     return `Failed to krs scan: ${error.message}`;
   }
 };
@@ -88,23 +93,23 @@ export const krsRecommend = async (ddClient: v1.DockerDesktopClient) => {
     await startKrsContainer(ddClient);
 
     // Run the Docker command using the Docker Desktop API
-    const output = await ddClient.docker.cli.exec("exec", [
+    const output = await ddClient.docker.cli.exec('exec', [
       containerID,
-      "krs", // Command to run inside the container
-      "recommend", // Arguments to the krs command
+      'krs', // Command to run inside the container
+      'recommend', // Arguments to the krs command
     ]);
 
     // Log the output to see the results
-    console.log("Command executed, output received: ", output);
+    console.log('Command executed, output received: ', output);
 
     if (output?.stderr) {
-      console.error("Error output from krs recommend:", output.stderr);
+      console.error('Error output from krs recommend:', output.stderr);
       return `Error: ${output.stderr}`;
     }
 
-    return output?.stdout || "No output received";
+    return output?.stdout || 'No output received';
   } catch (error: any) {
-    console.error("Error executing krs command:", error);
+    console.error('Error executing krs command:', error);
     return `Failed to krs recommend: ${error.message}`;
   }
 };
@@ -115,23 +120,23 @@ export const krsNamespaces = async (ddClient: v1.DockerDesktopClient) => {
     await startKrsContainer(ddClient);
 
     // Run the Docker command using the Docker Desktop API
-    const output = await ddClient.docker.cli.exec("exec", [
+    const output = await ddClient.docker.cli.exec('exec', [
       containerID,
-      "krs", // Command to run inside the container
-      "namespaces", // Arguments to the krs command
+      'krs', // Command to run inside the container
+      'namespaces', // Arguments to the krs command
     ]);
 
     // Log the output to see the results
-    console.log("Command executed, output received: ", output);
+    console.log('Command executed, output received: ', output);
 
     if (output?.stderr) {
-      console.error("Error output from krs namespaces:", output.stderr);
+      console.error('Error output from krs namespaces:', output.stderr);
       return `Error: ${output.stderr}`;
     }
 
-    return output?.stdout || "No output received";
+    return output?.stdout || 'No output received';
   } catch (error: any) {
-    console.error("Error executing krs command:", error);
+    console.error('Error executing krs command:', error);
     return `Failed to krs namespaces: ${error.message}`;
   }
 };
@@ -142,23 +147,23 @@ export const krsPods = async (ddClient: v1.DockerDesktopClient) => {
     await startKrsContainer(ddClient);
 
     // Run the Docker command using the Docker Desktop API
-    const output = await ddClient.docker.cli.exec("exec", [
+    const output = await ddClient.docker.cli.exec('exec', [
       containerID,
-      "krs", // Command to run inside the container
-      "pods", // Arguments to the krs command
+      'krs', // Command to run inside the container
+      'pods', // Arguments to the krs command
     ]);
 
     // Log the output to see the results
-    console.log("Command executed, output received: ", output);
+    console.log('Command executed, output received: ', output);
 
     if (output?.stderr) {
-      console.error("Error output from krs pods:", output.stderr);
+      console.error('Error output from krs pods:', output.stderr);
       return `Error: ${output.stderr}`;
     }
 
-    return output?.stdout || "No output received";
+    return output?.stdout || 'No output received';
   } catch (error: any) {
-    console.error("Error executing krs command:", error);
+    console.error('Error executing krs command:', error);
     return `Failed to krs pods: ${error.message}`;
   }
 };
@@ -169,23 +174,31 @@ export const krsExit = async (ddClient: v1.DockerDesktopClient) => {
     await startKrsContainer(ddClient);
 
     // Run the Docker command using the Docker Desktop API
-    const output = await ddClient.docker.cli.exec("exec", [
+    const output = await ddClient.docker.cli.exec('exec', [
       containerID,
-      "krs", // Command to run inside the container
-      "exit", // Arguments to the krs command
+      'krs', // Command to run inside the container
+      'exit', // Arguments to the krs command
     ]);
 
     // Log the output to see the results
-    console.log("Command executed, output received: ", output);
+    console.log('Command executed, output received: ', output);
 
     if (output?.stderr) {
-      console.error("Error output from krs recommend:", output.stderr);
+      console.error('Error output from krs recommend:', output.stderr);
       return `Error: ${output.stderr}`;
     }
 
-    return output?.stdout || "No output received";
+    // Stop the running container.
+    try {
+      await ddClient.docker.cli.exec('stop', [containerID]);
+      containerID=""
+    } catch (error: any) {
+      return `Failed to stop the running container: ${error.message}`
+    }
+
+    return output?.stdout || 'No output received';
   } catch (error: any) {
-    console.error("Error executing krs command:", error);
+    console.error('Error executing krs command:', error);
     return `Failed to krs exit: ${error.message}`;
   }
 };
@@ -195,7 +208,7 @@ export const krsHealth = async (ddClient: v1.DockerDesktopClient) => {
     // Ensure the container is running
     await startKrsContainer(ddClient);
   } catch (error: any) {
-    console.error("Error executing krs command:", error);
+    console.error('Error executing krs command:', error);
     return `Failed to krs health: ${error.message}`;
   }
 };
